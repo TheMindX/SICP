590a9806f0b3a01366b3e89de3b646d9b00f2641
TheMindX <lxf0525.1@gmail.com>
2015-04-07 22:25:03 +0800

    1.2.6

--- chapter1.rkt
+++ chapter1.rkt
@@ -17,7 +17,7 @@
     (sq_iter (/ x 2) x))
 
 
-(exact->inexact (my_sq 2))
+;(exact->inexact (my_sq 2))
 
 
 ;;;;power
@@ -57,8 +57,29 @@
       (square (fast_expt b (/ n 2)))
       (* b (fast_expt b (- n 1))))))
 
+;iter impl, just like fold,
+(define (fast_exp2 m n)
+  (let loop((m1 m) (n1 n) (rem 1) )
+    (let* ((mod (common.mod n1 2))
+          (div (/ (- n1 mod) 2))
+          (remIter (if (eq? mod 1) (* rem m1) rem)))
+      (if (eq? n1 1)
+          (* m1 rem)
+          (loop (common.square m1) div remIter)))))
 
-;(fast_expt 2 31)
+
+;(fast_exp2 2 31)
+
+(define (fast_mul a b)
+  (let loop ((a1 a) (b1 b) (count 0))
+    (let* ((mod (common.mod b1 2))
+           (div (/ (- b1 mod) 2))
+           (count1 (if (eq? mod 0) count (+ count a1))))
+      (if (eq? b1 1)
+          count1
+          (loop (+ a1 a1) div count1)))))
+          
+;(fast_mul 5 1024)
 
 ;;; sin 
 (define (mysin a)
@@ -69,4 +90,26 @@
         (- (* 3 sindiv3) (* 4 (cube sindiv3))))))
       
 
-(exact->inexact (mysin 5) )
+;(exact->inexact (mysin 5) )
+
+;note fast fib
+(define (fib n)
+  (define (iter fir sec iterTime)
+    (if (eq? iterTime n)
+        sec
+        (iter sec (+ fir sec) (+ iterTime 1))))
+  (iter 0 1 2))
+
+;(fib 115)
+    
+  
+;gcd
+(define (gcd m n)
+  (cond 
+    ((< m n) (gcd n m))
+    ((eq? m n) m)
+    ((eq? n 0) m)
+    (else (gcd n (common.mod m n)))))
+
+(gcd 16 8)
+
