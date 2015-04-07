1080801fbb015117fdda867eef81232fe361d59a
TheMindX <lxf0525.1@gmail.com>
2015-04-07 16:29:31 +0800

    mysin.rkt

---
+++ mysin.rkt
@@ -0,0 +1,11 @@
+#lang racket
+
+(define (mysin a)
+  (define (cube x) (* x x x))
+  (if (< a 0.001)
+      a
+      (let ((sindiv3 (mysin (/ a 3))))
+        (- (* 3 sindiv3) (* 4 (cube sindiv3))))))
+      
+
+(exact->inexact (mysin 5) )
