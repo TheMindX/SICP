7c8294c151fb3258ee68a10930831927ad5c3b3f
xf <lxf0525@gmail.com`>
2015-07-30 14:58:35 +0800

    stream
    chapter3 is done

--- chapter3.rkt
+++ chapter3.rkt
@@ -211,5 +211,69 @@
 ;3.24
 
   
+;stream
+(define-syntax-rule (mdelay exp)
+    (let ((v null))
+      (lambda ()
+        (if (null? v)
+            (begin (set! v exp) v)
+            v))))
+
+
+(define-syntax-rule (mforce lv)
+   (lv))
+
+(define-syntax-rule (cons-stream a b)
+  (cons a (mdelay b)))
+
+(define (stream-car stream)
+  (car stream))
+
+(define (stream-cdr stream)
+  (mforce (cdr stream)))
+
+(define (make-mstream init fnext)
+  (cons-stream init (make-mstream (fnext init) fnext)))
+
+
+(define (integers-from n)
+  (cons-stream n (integers-from (+ n 1))))
+
+(define integers (integers-from 1))
+
+
+(define (stream-filter stream pred)
+  (let find ((test-stream stream))
+    (let ((first (stream-car test-stream)))
+      (if (pred first)
+        (cons-stream first 
+                     (stream-filter (stream-cdr test-stream) pred))
+        (find (stream-cdr test-stream))))))
+  
+
+
+(stream-car (stream-cdr (stream-cdr (stream-cdr (stream-cdr integers)))))
+
+(define (print_stream stream n)
+  (let loop ((i  n) 
+             (first (stream-car stream) )
+             (rest (stream-cdr stream)))
+    (if (eq? i 0)
+        null
+        (if (eq? rest null)
+            null
+            (begin (common.println first)
+                   (loop (- i 1) (stream-car rest) (stream-cdr rest)))))))
+        
+             
   
   
+;now let we find primary
+
+(define (sieve stream)
+  (cons-stream
+   (stream-car stream)
+   (sieve (stream-filter stream
+                         (lambda (n) (not (eq? 0 (modulo n (stream-car stream)))))))))
+   
+   
